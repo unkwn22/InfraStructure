6d1e70c6d33a3d867e24fb71ecfec5ac698d814a
Jason Choi <lsjc12911@naver.com>
2021-08-21 23:45:06 +0900

    feat: entity mapped

---
+++ InfraStructure/src/main/java/com/example/InfraStructure/entity/Post.java
@@ -0,0 +1,47 @@
+package com.example.InfraStructure.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.example.InfraStructure.entity.Time.ByEntity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Builder
+public class Post extends ByEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "post_id")
+	private Long postId;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "users_id")
+	private Users users;
+
+	@Column(name = "post_content")
+	private String postContent;
+	
+	public void updatePost(Post post) {
+		this.postContent = post.getPostContent();
+	}
+	
+	public void addUser(Users users) {
+		this.users = users;
+		users.getPosts().add(this);
+	}
+}
--- InfraStructure/src/main/java/com/example/InfraStructure/entity/Users.java
+++ InfraStructure/src/main/java/com/example/InfraStructure/entity/Users.java
@@ -1,23 +1,47 @@
 package com.example.InfraStructure.entity;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+
+import com.example.InfraStructure.entity.Time.TimeEntity;
 
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Builder
 @Getter
 @Setter
-public class Users {
+@Builder
+public class Users extends TimeEntity {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long userId;
+	@Column(name = "users_id")
+	private Long userId;
 	
+	@Column(name = "username")
 	private String username;
+	
+	@Column(name = "password")
 	private String password;
+	
+	@Email
+	@Column(name = "email_address")
 	private String email;
+	
+	@Column(name = "role")
+	private String roles;
+	
+	@OneToMany
+	@Builder.Default
+	List<Post> posts = new ArrayList<>();
+	
+	public void updateUser() {
+		
+	}
 	
 }
